d4385f74caf19e4723f59877c2365a7d2779f62b
AER-Shalaan <abdelrahman.shalaan03@gmail.com>
2024-02-23 01:30:12 +0200

    + finish SharedPreferences

--- lib/providers/settings_provider.dart
+++ lib/providers/settings_provider.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsProvider extends ChangeNotifier{
   ThemeMode theme = ThemeMode.light;
   String language = "en";
-  void changeTheme(ThemeMode newThem){
-    if(theme == newThem) return;
-    theme = newThem;
+
+  void changeTheme(ThemeMode newTheme)async{
+    if(theme == newTheme) return;
+    final SharedPreferences _prefs = await SharedPreferences.getInstance();
+    if(newTheme == ThemeMode.dark){
+      _prefs.setBool("Theme", true);
+    }else{
+      _prefs.setBool("Theme", false);
+    }
+    theme = newTheme;
     notifyListeners();
   }
-  void changeLanguage(String newLanguage){
+
+  void changeLanguage(String newLanguage) async{
     if(language == newLanguage) return;
+    final SharedPreferences _prefs = await SharedPreferences.getInstance();
+    await _prefs.setString("Language", newLanguage);
     language = newLanguage;
     notifyListeners();
   }
 
+initialize() async{
+  final SharedPreferences _prefs = await SharedPreferences.getInstance();
+  language = _prefs.getString("Language")??"en";
+  theme=_prefs.getBool("Theme")==true?ThemeMode.dark:ThemeMode.light;
+  notifyListeners();
+}
 }
